6b1133f99af8bc99ac82fe372ca4fff5ccdee033
周浩 <zh.sqy@qq.com>
2016-05-06 10:42:42 +0800

    新增自动配置类

---
+++ hsweb-platform-ui-miniui/src/main/java/org/hsweb/platform/ui/UIAutoConfiguration.java
@@ -0,0 +1,12 @@
+package org.hsweb.platform.ui;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by zhouhao on 16-5-6.
+ */
+@Configuration
+@ComponentScan("org.hsweb.platform.ui")
+public class UIAutoConfiguration {
+}
