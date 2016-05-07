cdd87bf4261fbb93b3f7022b792970823618541f
周浩 <zh.sqy@qq.com>
2016-05-07 15:02:49 +0800

    新增权限控制

--- hsweb-platform-ui-miniui/src/main/java/org/hsweb/platform/ui/PageViewController.java
+++ hsweb-platform-ui-miniui/src/main/java/org/hsweb/platform/ui/PageViewController.java
@@ -1,10 +1,9 @@
 package org.hsweb.platform.ui;
 
-import org.hsweb.web.authorize.annotation.Authorize;
-import org.hsweb.web.bean.common.QueryParam;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.hsweb.web.core.authorize.ExpressionScopeBean;
+import org.hsweb.web.core.authorize.annotation.Authorize;
+import org.hsweb.web.core.utils.WebUtil;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,23 +16,32 @@ import java.util.Map;
  * Created by zhouhao on 16-4-8.
  */
 @Controller
-@Authorize
-public class PageViewController {
+public class PageViewController implements ExpressionScopeBean {
 
+    @RequestMapping(value = "/admin/login.html", method = RequestMethod.GET)
+    public ModelAndView login() {
+        return  new ModelAndView("admin/login");
+    }
 
-    @RequestMapping(value = "/admin/**/*.html",method = RequestMethod.GET)
+    @RequestMapping(value = "/admin/**/*.html", method = RequestMethod.GET)
+    @Authorize(expression = "#user.username=='admin'||#user.getModuleByUri(#pageViewController.getUri(#request))!=null")
     public ModelAndView view(HttpServletRequest request,
                              @RequestParam(required = false) Map<String, Object> param) {
+        String path = getUri(request);
+        if (path.contains("."))
+            path = path.split("[.]")[0];
+        ModelAndView modelAndView = new ModelAndView(path);
+        modelAndView.addObject("param", param);
+        modelAndView.addObject("absPath", WebUtil.getBasePath(request));
+        return modelAndView;
+    }
+
+    public String getUri(HttpServletRequest request) {
         String path = request.getRequestURI();
         String content = request.getContextPath();
         if (path.startsWith(content)) {
             path = path.substring(content.length() + 1);
         }
-        if (path.contains("."))
-            path = path.split("[.]")[0];
-        ModelAndView modelAndView = new ModelAndView(path);
-        modelAndView.addObject("param", param);
-
-        return modelAndView;
+        return path;
     }
 }
