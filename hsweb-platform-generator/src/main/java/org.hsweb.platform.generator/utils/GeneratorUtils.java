c686d67362cc80b5423706105fb4bde750af480f
周浩 <zh.sqy@qq.com>
2016-07-08 09:35:49 +0800

    新增表结构SQL创建方法

--- hsweb-platform-generator/src/main/java/org.hsweb.platform.generator/utils/GeneratorUtils.java
+++ hsweb-platform-generator/src/main/java/org.hsweb.platform.generator/utils/GeneratorUtils.java
@@ -1,6 +1,18 @@
 package org.hsweb.platform.generator.utils;
 
 import org.hsweb.commons.StringUtils;
+import org.hsweb.ezorm.executor.SQL;
+import org.hsweb.ezorm.meta.DatabaseMetaData;
+import org.hsweb.ezorm.meta.FieldMetaData;
+import org.hsweb.ezorm.meta.TableMetaData;
+import org.hsweb.ezorm.render.SqlRender;
+import org.hsweb.ezorm.render.dialect.H2DatabaseMeta;
+import org.hsweb.ezorm.render.dialect.MysqlDatabaseMeta;
+import org.hsweb.ezorm.render.dialect.OracleDatabaseMeta;
+
+import java.sql.JDBCType;
+import java.util.List;
+import java.util.Map;
 
 public class GeneratorUtils {
     public String getGetter(String name, String javaType) {
@@ -14,4 +26,42 @@ public class GeneratorUtils {
         return "set" + StringUtils.toUpperCaseFirstOne(name);
     }
 
+    public String buildCreateSQL(String dbType, String tableName, String comment, List<Map<String, Object>> fields) {
+        DatabaseMetaData databaseMetaData;
+        switch (dbType) {
+            case "h2":
+                databaseMetaData = new H2DatabaseMeta();
+                break;
+            case "oracle":
+                databaseMetaData = new OracleDatabaseMeta();
+                break;
+            case "mysql":
+                databaseMetaData = new MysqlDatabaseMeta();
+                break;
+            default:
+                return "";
+        }
+        databaseMetaData.init();
+        SqlRender render = databaseMetaData.getRenderer(SqlRender.TYPE.META_CREATE);
+        TableMetaData metaData = new TableMetaData();
+        metaData.setName(tableName);
+        metaData.setComment(comment);
+        fields.forEach(map -> {
+            FieldMetaData fieldMetaData = new FieldMetaData();
+            String name = (String) map.get("column");
+            if (name == null) {
+                name = (String) map.get("name");
+            }
+            fieldMetaData.setName(name);
+            fieldMetaData.setComment((String) map.get("comment"));
+            fieldMetaData.setDataType((String) map.get("dataType"));
+            fieldMetaData.setJdbcType(JDBCType.valueOf((String) map.get("jdbcType")));
+            metaData.addField(fieldMetaData);
+        });
+        SQL sql = render.render(metaData, metaData);
+        StringBuilder builder = new StringBuilder();
+        builder.append(sql.getSql()).append(";\n");
+        sql.getBinds().forEach(bindSQL -> builder.append(bindSQL.getSql().getSql()).append(";\n"));
+        return builder.toString();
+    }
 }
